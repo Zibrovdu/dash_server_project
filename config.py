cfff0b6a4637957a97de282120885fde2042d345
Dmitriy Zibrov <zibrovdu@gmail.com>
2021-02-04 10:21:31 +0300

    Test app on server.

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+from sqlalchemy import create_engine
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class BaseConfig:
-    SQLALCHEMY_DATABASE_URI = f'postgres://postgres:6002815@192.168.1.4:5432/test'
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:6002815@192.168.1.4:5432/test'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my secret key'
+    engine = create_engine('postgres://postgres:6002815@192.168.1.4:5432/test')
