2cfa5c3669002011845c0d2b5e8ecee3fc52a613
Dmitriy Zibrov <zibrovdu@gmail.com>
2021-02-04 12:01:29 +0300

    current project to test server

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,4 @@
-from app import create_app
-
-server = create_app()
+from test_app import app as application
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0')
-
-
-# from myproject import app as application
-#
-# if __name__ == "__main__":
-#     application.run(host='0.0.0.0')
+    application.run_server(host='0.0.0.0')
